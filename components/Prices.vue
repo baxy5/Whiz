15c2b5a65f4d7ce25ac375127629d6fc7f5b7195
baxy5 <bakos.soda@gmail.com>
2024-01-15 16:09:34 +0100

    tag

--- components/Prices.vue
+++ components/Prices.vue
@@ -11,8 +11,11 @@
       <div id="prices" class="mt-4 min-w-min flex gap-4 lg:grid lg:grid-cols-2">
         <Box
           box-type="dark"
-          custom-class="custom-border flex flex-col justify-between items-center pt-[3rem] pb-[1rem] sl:px-4"
+          custom-class="custom-border relative flex flex-col justify-between items-center pt-[3rem] pb-[1rem] sl:px-4"
         >
+          <div class="tag">
+            <p class="tag-text">Ingyenes konzultáció</p>
+          </div>
           <h4 class="title pb-5 text-center w-[20rem] md:w-[25rem]">
             webfejlesztés
           </h4>
@@ -175,9 +178,34 @@ onNuxtReady(async () => {
   gap: 1rem;
 }
 
-.custom-border {
-  /* border: 1px solid #5e2bff; */
+.tag {
+  position: absolute;
+  top: -1rem;
+  color: #fff;
+  background-color: #000;
+  border: 1px solid #5e2bff;
+  border-radius: 4.375rem;
+}
+.tag-text {
+  color: #fff;
+  font-size: 0.9375rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
 
+  padding-top: 0.3rem;
+  padding-bottom: 0.3rem;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+
+  background: linear-gradient(90deg, #5e2bff 0%, #723080 115.64%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.custom-border {
+  position: relative;
   background: linear-gradient(#000, #000) padding-box,
     linear-gradient(to right, #5e2bff, #b64ccd) border-box;
   border: 1px solid transparent;
