3da8d545a675e6de6d908140cccce2fe026f7833
baxy5 <bakos.soda@gmail.com>
2024-01-17 22:45:54 +0100

    hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,40 +2,46 @@
   <section class="pt-3 lg:pt-14">
     <Box
       box-type="fancy"
-      custom-class="pt-[7.5rem] pb-[3rem] px-[1.5rem]
+      custom-class="grid grid-cols-2
+      pt-[7.5rem] pb-[3rem] px-[1.5rem]
     lg:pt-[10rem] lg:px-[7rem] xl:pt-[13rem] xl:px-[12rem]"
     >
-      <Indicator>
-        <p>{{ indicatorText }}</p>
-        <p class="flex gap-1 items-center">
-          4.8
+      <div>
+        <Indicator>
+          <p>{{ indicatorText }}</p>
+          <p class="flex gap-1 items-center">
+            {{ indicatorDetail }}
+            <img
+              class="w-3 h-3 lg:w-[0.85rem] lg:h-[0.85rem] xl:w-[1rem] xl:h-[1rem]"
+              :src="indicatorSrc"
+              :alt="indicatorAlt"
+            />
+          </p>
+        </Indicator>
+        <h1 class="text-white font-normal leading-normal pt-14 pb-12">
+          <span
+            class="text-[1.5rem] md:text-[2rem] lg:text-[3rem] xl:text-[3.5rem]"
+            >{{ spanOne }}</span
+          >
+          <br />
+          <span
+            class="text-[1.875rem] pt-2 md:text-[2.5rem] lg:text-[3.5rem] xl:text-[4rem]"
+            >{{ spanTwo }}</span
+          >
+        </h1>
+        <p
+          v-html="formattedDescription"
+          class="text-[1.125rem] text-white font-normal leading-normal lg:w-[50rem] xl:w-[60rem]"
+        ></p>
+        <div class="flex justify-center pt-10 lg:pt-36 xl:pt-48">
           <img
-            class="w-3 h-3 lg:w-[0.85rem] lg:h-[0.85rem] xl:w-[1rem] xl:h-[1rem]"
-            src="/icons/star-icon.svg"
-            alt="Star Icon - Jelzi a visszajelzések értékelését ami 4.8"
+            src="/icons/arrow-down.svg"
+            alt="Arrow Down - Tudsz lefele görgetni."
           />
-        </p>
-      </Indicator>
-      <h1 class="text-white font-normal leading-normal pt-14 pb-12">
-        <span
-          class="text-[1.5rem] md:text-[2rem] lg:text-[3rem] xl:text-[3.5rem]"
-          >{{ spanOne }}</span
-        >
-        <br />
-        <span
-          class="text-[1.875rem] pt-2 md:text-[2.5rem] lg:text-[3.5rem] xl:text-[4rem]"
-          >{{ spanTwo }}</span
-        >
-      </h1>
-      <p
-        v-html="formattedDescription"
-        class="text-[1.125rem] text-white font-normal leading-normal lg:w-[50rem] xl:w-[60rem]"
-      ></p>
-      <div class="flex justify-center pt-10 lg:pt-36 xl:pt-48">
-        <img
-          src="/icons/arrow-down.svg"
-          alt="Arrow Down - Tudsz lefele görgetni."
-        />
+        </div>
+        <div v-if="showcaseImageOneSrc">
+          <img :src="showcaseImageOneSrc" />
+        </div>
       </div>
     </Box>
     <div></div>
@@ -45,9 +51,15 @@
 <script setup lang="ts">
 const props = defineProps<{
   indicatorText: string;
+  indicatorSrc: string;
+  indicatorDetail?: string;
+  indicatorAlt?: string;
   spanOne: string;
   spanTwo: string;
   heroText: string;
+  showcaseImageOneSrc?: string;
+  showcaseImageTwoSrc?: string;
+  showcaseImageThreeSrc?: string;
 }>();
 
 const descriptionSentence = ref<string>(props.heroText || "");
--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,13 @@
     <Navbar />
     <Hero
       indicator-text="Ügyfél elégedettség"
+      indicator-src="/icons/star-icon.svg"
+      indicator-detail="4.8"
+      indicator-alt="Star Icon - Jelzi a visszajelzések értékelését ami 4.8"
       span-one="Együtt alkotunk."
       span-two="Együtt szárnyalunk."
       hero-text="Az /tonline /tjelenlét kulcsfontosságú. Képzelj csak el egy olyan /tweboldalt, amely pontosan tükrözi vállalkozásodat. /tÜgyfélbarát hozzáállásunk és a /t100%-os elégedettség elérése érdekében tett elkötelezettségünk tesz minket különlegessé."
+      showcase-image-one-src="~/assets/images/customer-ani.svg"
     />
     <Services />
     <Testimonials />
