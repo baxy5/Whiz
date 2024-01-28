e33d84c3451ad67f42e789be60faa03c7bd4bde3
baxy5 <bakos.soda@gmail.com>
2024-01-28 22:14:40 +0100

    stack

--- components/dev/Stack.vue
+++ components/dev/Stack.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="pt-8">
-    <div>
+    <div class="md:hidden">
       <div class="px-4">
         <h3 class="title">Mi hiszünk abban, hogy bármi megoldható...</h3>
       </div>
@@ -41,11 +41,11 @@
             </li>
           </ul>
         </div>
-        <div class="pt-4 pl-14 sl:pl-24 2sl:pl-36">
+        <div class="z-[9999] pt-4 pl-14 sl:pl-24 2sl:pl-36">
           <a class="button" href="#contact">Érdekel</a>
         </div>
         <img
-          class="strapi-mock absolute h-auto w-[65rem] -top-24 sl:left-24 sl:-top-16 2sl:-top-24 2sl:w-[75rem] 2sl:left-44"
+          class="strapi-mock -z-[9999] absolute h-auto w-[65rem] -top-24 sl:left-24 sl:-top-16 2sl:-top-24 2sl:w-[75rem] 2sl:left-44"
           src="/images/strapi-mock.png"
           alt="Strapi Mock Image"
         />
@@ -65,13 +65,89 @@
         <div
           class="flex gap-2 items-center text-white text-[0.875rem] leading-normal font-bold lg:text-[1rem]"
         >
-          <a href="tel:+36308754438">Megnézem</a>
+          <a href="/penzugy">Megnézem</a>
           <img
             src="/icons/arrow-right.svg"
             alt="Arrow Right - Kattintható, hogy többet megtudj."
           />
         </div>
       </GlassBox>
+    </div>
+    <div class="hidden relative md:flex md:gap-4 lg:pb-16 xl:pb-44">
+      <GlassBox
+        custom-class="relative w-[25rem] h-[17rem] px-[2.8rem] pt-[2rem] pb-[2rem] lg:w-[30rem] lg:h-[19rem] xl:w-[35rem] xl:h-full"
+      >
+        <h2
+          class="text-white text-[1.125rem] font-bold leading-normal pb-[1.3rem] md:text-[1.25rem] lg:text-[1.5rem]"
+        >
+          Weboldal hitelre?
+        </h2>
+        <p
+          class="text-white text-[0.8125rem] font-normal leading-normal pb-[2.2rem] md:text-[0.9rem] lg:text-[1rem]"
+        >
+          Pénzügyi tanácsadóink a legkedvezőbb feltételek mellett tudnak
+          támogatni weboldalad, céged vagy vállalkozásod létrjöttében.
+        </p>
+        <div
+          class="flex gap-2 items-center text-white text-[0.875rem] leading-normal font-bold lg:text-[1rem]"
+        >
+          <a href="/penzugy">Megnézem</a>
+          <img
+            src="/icons/arrow-right.svg"
+            alt="Arrow Right - Kattintható, hogy többet megtudj."
+          />
+        </div>
+      </GlassBox>
+      <div class="box-big w-full col-span-2">
+        <h3 class="title-big pb-8">
+          Mi hiszünk abban, hogy bármi megoldható...
+        </h3>
+        <ul>
+          <div class="lg:flex lg:flex-col lg:gap-8 xl:flex xl:flex-row xl:gap-16">
+            <div class="lg:grid lg:gap-8">
+              <li>
+                <img src="/icons/list-dot.svg" alt="Lista Jelzés Icon" />
+                Egyedi Web App Fejlesztés
+              </li>
+              <li>
+                <img src="/icons/list-dot.svg" alt="Lista Jelzés Icon" />
+                Webdesign és Fejlesztés
+              </li>
+              <li>
+                <img src="/icons/list-dot.svg" alt="Lista Jelzés Icon" />
+                Egyedi animációk
+              </li>
+              <li>
+                <img src="/icons/list-dot.svg" alt="Lista Jelzés Icon" />
+                Ecommerce Fejlesztés
+              </li>
+            </div>
+            <div class="lg:grid lg:gap-8">
+              <li>
+                <img src="/icons/list-dot.svg" alt="Lista Jelzés Icon" />
+                Egyedi CMS
+              </li>
+              <li>
+                <img src="/icons/list-dot.svg" alt="Lista Jelzés Icon" />
+                Reszponzív webfejlesztés
+              </li>
+              <li>
+                <img src="/icons/list-dot.svg" alt="Lista Jelzés Icon" />
+                SEO
+              </li>
+              <li>
+                <img src="/icons/list-dot.svg" alt="Lista Jelzés Icon" />
+                Google Tag Manager
+              </li>
+            </div>
+          </div>
+        </ul>
+      </div>
+      <img
+        class="strapi-mock -z-[9999] absolute h-auto w-[50rem] top-44 left-0 lg:w-[70rem] lg:top-32 lg:left-0 xl:-top-12"
+        src="/images/strapi-mock.png"
+        alt="Strapi Mock Image"
+      />
     </div>
   </section>
 </template>
@@ -117,21 +193,47 @@ li {
   -webkit-text-fill-color: transparent;
 }
 .button {
-  padding: 0.25rem 1.25rem 0.25rem 1.25rem;
+  padding: 0.35rem 1.25rem 0.35rem 1.25rem;
   border-radius: 4.375rem;
   border: 1px solid #6d6d6d;
   background: rgba(72, 72, 72, 0.75);
   backdrop-filter: blur(2px);
   color: #fff;
   font-family: "Space Grotesk";
-  font-size: 0.6875rem;
+  font-size: 0.75rem;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
   text-transform: uppercase;
+  cursor: pointer;
+  transition: all 0.1s ease-in-out;
+}
+.button:hover {
+  background-color: #5e2bff;
+  border: 1px solid #5e2bff;
 }
 img {
   max-width: none;
+}
+.title-big {
+  font-family: "Space Grotesk";
+  font-size: 2.125rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  background: linear-gradient(90deg, #5e2bff 0.02%, #b64ccd 75.54%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.box-big {
+  fill: rgba(29, 29, 29, 0.7);
+  stroke-width: 1px;
+  stroke: #535353;
+  backdrop-filter: blur(25px);
+  border: 1px solid #535353;
+  border-radius: 1.25rem;
+  padding: 0.5rem 1rem 0.5rem 1rem;
 }
 @media only screen and (min-width: 414px) {
   .title {
@@ -147,6 +249,11 @@ img {
   }
   li {
     font-size: 1.5rem;
+  }
+}
+@media only screen and (min-width: 1024px){
+  .title-big{
+    font-size: 2.5rem;
   }
 }
 </style>
