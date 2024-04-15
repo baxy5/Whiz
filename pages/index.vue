160c6ab06b839a4f37fb928575fcbb2074a46e1a
baxy5 <bakos.soda@gmail.com>
2024-04-15 21:56:42 +0200

    asd

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="relative px-3 xxl:container overflow-hidden">
-    <CookieConsent />
+    <!-- <CookieConsent /> -->
     <Navbar />
     <Hero
       indicator-text="Ügyfél elégedettség"
